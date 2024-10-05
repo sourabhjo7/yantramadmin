b200d2f84d82250fa6a5528ab0d8cbfa3232005f
sourabh joshi <20ucc103@lnmiit.ac.in>
2024-10-06 04:49:38 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',  // Proxy requests from your frontend
+        destination: 'http://13.126.92.193:5000/api/v1/:path*',  // Redirect them to your HTTP backend
+      },
+    ];
+  },
+};
 
 export default nextConfig;
